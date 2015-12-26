73bb256394b8b2ac9065d07349acb42a78edc41a
Chris G <cgathercole@gmail.com>
2015-12-26 22:25:55 +0000

    MODIFIED: iterative lengthen/shorten

--- chris_sequence.py
+++ chris_sequence.py
@@ -3,20 +3,23 @@ import time
 
 #bb = BlinkyTape('/dev/ttyACM0')
 bb = BlinkyTape('COM5')
+led_count = 60
+pixel_full_on = [100, 100, 100]
+pixel_off     = [0, 0, 0]
 
+def clear_all():
+	for x in range(60):
+		bb.sendPixel(0, 0, 0)
+	bb.show()
+
+clear_all()
 while True:
-	for x in range(60):
-		bb.sendPixel(0, 0, 0)
-	bb.show()
 
-	for s in range(60):
-		for x in range(s):
-			bb.sendPixel(100, 100, 100)
-		bb.show()
-		time.sleep(.5)
+	for s in range(led_count):
+		for t in range(s+1):
+			pixel_list = map(lambda x: pixel_full_on if x<=t else pixel_off, range(led_count))
+			bb.send_list(pixel_list)
 
-	for x in range(60):
-		bb.sendPixel(0, 0, 0)
-	bb.show()
-
-	time.sleep(.5)
+		for t in range(s, -1, -1):
+			pixel_list = map(lambda x: pixel_full_on if x<=t else pixel_off, range(led_count))
+			bb.send_list(pixel_list)
